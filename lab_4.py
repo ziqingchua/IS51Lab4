c031cbe51abea17dbd0cfdffc2f01a35557b3d10
Zi Qing Chua <ziqingchua@swuser-129-8-194-160.swlan.csufresno.edu>
2022-05-15 03:42:07 -0700

    finish lab 4

--- lab_4.py
+++ lab_4.py
@@ -35,3 +35,21 @@ main
 """
 
 
+def main():
+    question = "What is the capital of California? "
+    answer = "Sacramento"
+    ask(question, answer)
+
+def ask(question, answer, max_tries=3) :
+    tries = 0 
+    ans = " "
+    while tries < max_tries:
+        tries = tries + 1 
+        ans = input(question)
+        if ans == answer:
+            print("Correct!")
+            break 
+    if ans != answer:
+        print ("You have used up your allotment of guesses.")
+
+main 
